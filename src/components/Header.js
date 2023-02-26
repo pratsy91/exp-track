1833591254abece5a66e605a844fa5b544267f00
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-02-26 15:41:43 +0530

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,90 @@
 import React, { useContext } from "react";
 import { Button, Nav, Navbar } from "react-bootstrap";
-import { NavLink, useNavigate } from "react-router-dom";
-import AuthContext from "../store/auth-context";
+import { useSelector } from "react-redux";
+import { NavLink, useNavigate, useRouteLoaderData } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
-  const authCtx = useContext(AuthContext);
+  const theme = useSelector((state) => state.themeReducer.theme);
+  const token = useRouteLoaderData("token");
+
+  const loginHandler = () => {
+    navigate("/auth?mode=login");
+  };
+
   const logoutHandler = () => {
-    authCtx.logOut();
-    navigate("/");
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    navigate("/auth?mode=login");
   };
   return (
     <React.Fragment>
-      <Navbar bg="white" variant="light">
-        <Navbar.Brand className="ms-5">My Expense Tracker</Navbar.Brand>
-        <Nav>
-          <NavLink
-            className={({ isActive }) =>
-              isActive
-                ? "text-decoration-none text-blue"
-                : "text-decoration-none text-black"
-            }
+      <Navbar
+        style={
+          theme
+            ? { backgroundColor: "#350241" }
+            : { backgroundColor: "#af05d9" }
+        }
+      >
+        <Navbar.Brand
+          className={
+            theme
+              ? "ms-5 bg- p-2 rounded text-white"
+              : "ms-5 bg- p-2 rounded text-white"
+          }
+        >
+          My Expense Tracker
+        </Navbar.Brand>
+        {token && (
+          <Nav>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-decoration-none text-warning me-3"
+                  : "text-decoration-none text-white me-3"
+              }
+              end
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-decoration-none text-warning me-3"
+                  : "text-decoration-none text-white me-3"
+              }
+            >
+              Profile
+            </NavLink>
+            <NavLink
+              to="/expenses"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-decoration-none text-warning"
+                  : "text-decoration-none text-white"
+              }
+            >
+              Expenses
+            </NavLink>
+          </Nav>
+        )}
+        {!token && (
+          <Button
+            className="ms-auto me-5"
+            onClick={loginHandler}
+            variant={theme ? "info" : "dark"}
           >
-            Home
-          </NavLink>
-        </Nav>
-        {authCtx.isLoggedin && (
-          <Button className="ms-auto me-5" onClick={logoutHandler}>
+            Login
+          </Button>
+        )}
+        {token && (
+          <Button
+            className="ms-auto me-5"
+            onClick={logoutHandler}
+            variant={theme ? "info" : "dark"}
+          >
             Logout
           </Button>
         )}
